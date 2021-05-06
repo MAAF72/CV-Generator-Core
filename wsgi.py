aac0b85e2f370f1f0864f5d38064e49e5789a0f0
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2021-05-06 11:11:50 +0700

    fix host binding in app

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@ from pyppeteer import chromium_downloader
 from app.main import app
 
 if __name__ == '__main__':
+    print('Downloading chromium for pyppeteer')
     chromium_downloader.download_chromium()
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
